142826c49243a460374037c4628afb63735f0557
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-16 10:42:10 -0500

    Whoops, I was doing the wrong exercise before. This is the first submission with the correct exercise - and I have created the prompts for users to insert starting and ending numbers.

--- for.php
+++ for.php
@@ -1,5 +1,30 @@
 <?php
 
-for ($a = 1; $a <= 100; $a++) {
-    echo "{$a}\n";
-}
+	fwrite(STDOUT, 'Pick a starting number... ');
+	fwrite(STDOUT, 'Pick an ending number... ');
+	$userGuess = fgets(STDIN);
+		
+
+
+
+
+// Create a file named for.php in your exercises repo. Commit and push all changes after each step.
+
+// Prompt user for a starting number and ending number, then display all the numbers from the starting to ending using a for loop.
+
+// Refactor to allow user to choose increment. (count by 1, 2, 10, ...)
+
+// Default increment to 1 if no input.
+
+// Make sure you are only allowing users to pass in numbers. Give an error message is both passed arguments are not numeric. See php.net/is_numeric.
+
+
+
+
+
+
+
+
+// for ($a = 1; $a <= 100; $a++) {
+//     echo "{$a}\n";
+// }
