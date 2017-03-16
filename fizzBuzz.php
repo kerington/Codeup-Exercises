8ca604fe43c6ef70ea5ff204ff65f98c29abdd6b
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-16 11:27:34 -0500

    Completed FizzBuzz exercise!

--- fizzBuzz.php
+++ fizzBuzz.php
@@ -2,5 +2,15 @@
 
 
 	for ($a = 1; $a <= 100; $a++) {
-	    echo "{$a}\n";	
-	}
+	    // echo "{$a}\n";	
+		
+		if ( $a % 3 == 0 && $a % 5 !=0 ) {
+	            echo ("Fizz\n");
+	        } elseif ( $a % 5 == 0 && $a % 3 !=0) {
+	            echo ("Buzz\n");
+	        } elseif ( $a % 15 == 0) {
+	            echo ("FizzBuzz\n");
+	        } else {
+	        echo ("{$a}\n");
+	    	}
+    } 
