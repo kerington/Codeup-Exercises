c46392777aea46234687fbc5c608c2f9382e64c5
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-20 11:43:19 -0500

    Adding Search Arrays exercise with steps to complete.

---
+++ search-arrays.php
@@ -0,0 +1,16 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+
+
+
+
+
+// Download and save search-arrays.php to your exercises directory. Commit each step in git and push to GitHub at the end of the exercise.
+
+// Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+
+// Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
