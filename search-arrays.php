c6f2ecd38251138c8a5805fce93b6e27398be77e
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-03-20 12:09:37 -0500

    COMPLETED: Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.

--- search-arrays.php
+++ search-arrays.php
@@ -1,16 +1,18 @@
 <?php
 
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
-
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+$searchFor = 'Bob';
 
-
-
-
-
-
-// Download and save search-arrays.php to your exercises directory. Commit each step in git and push to GitHub at the end of the exercise.
 
 // Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+$result = array_search($searchFor, $names);
+
+if ($result !== false) {
+    echo "true" . PHP_EOL;
+} else {
+	echo "false" . PHP_EOL;
+}
+
 
 // Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
