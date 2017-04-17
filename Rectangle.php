7933b54e8831c4594f5428977223564cf92d4e7b
Keri C <Kerington@Keris-MacBook-Pro.local>
2017-04-17 12:19:55 -0500

    Added "private" to rectangle width and height

--- Rectangle.php
+++ Rectangle.php
@@ -4,8 +4,8 @@
 // the properties $height and $width
 class Rectangle {
 
-	public $height;
-	public $width;
+	private $height;
+	private $width;
 
 // a constructor to set the height and width on instantiation
 	public function _construct($height, $width) {
@@ -25,21 +25,20 @@ class Rectangle {
 
 
 
+// DONE Update the Rectangle class from the previous lesson to contain private properties for height and width.
+
+// Before testing the Rectangle and Square classes, try to think of the outcomes before executing. Will the Rectangle class work as before? What about the Square class? Create objects from both classes and execute the area() method. Did the result meet your expected outcome?
+
+// Update the height and width properties of the Rectangle class to have a visibility of protected. Repeat the previous step. Did the result meet your expected outcome this time?
+
+// Change the visibility back to private for both properties. Create the necessary getters and setters for the Rectangle and Square classes to work as desired with only private properties.
+
+// Open your Log.php in Sublime. Refactor this class to:
+
+// limit the visibility of the $handle and $filename properties.
+// using setters, ensure that the filename is a string
+// ensure that users cannot manipulate $handle once the object is instantiated.
+// Bonus: In your setter for $filename, use a combination of touch() and is_writable() to ensure you can write to the specified location. What do you think you class should do if this fails?
 
 
-
-
-// DONE 3. Create a shapes_test.php file next to Rectangle.php. In shapes_test.php make sure to require_once Rectangle.php.
-
-// DONE 4. Test your new class by creating an instance of Rectangle with various heights and widths. Calling the area method should correctly display the product of height and width. 
-
-// DONE 5. Create a new file named Square.php 
-
-// DONE 6. In this file create a class named Square that extends Rectangle 
-
-// DONE 7. In the new Square class, create a method called perimeter that returns the perimeter of the square. 
-
-// DONE 8. Test your new Square class by creating an instance of Square with various matching height and width. Calling the area() method should correctly display the product of height and width. Invoking the new method perimeter() should display the perimeter of the square. 
-
-// 9. What could we do to ensure the square class was actually computing values for a square? What prevents the height and width from being different? Would it be more beneficial if we could have a different constructor for our Square class?
 ?>
